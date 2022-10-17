a4b114643e67e6587f0c4a76d8d72443243ed0c8
Sean7ee <lee.sean1103@gmail.com>
2022-10-17 01:20:54 -0400

    added longest prefix match finder method

--- router/sr_rt.h
+++ router/sr_rt.h
@@ -12,25 +12,25 @@
 /* ----------------------------------------------------------------------------
  * struct sr_rt
  *
- * Node in the routing table 
+ * Node in the routing table
  *
  * -------------------------------------------------------------------------- */
 
-struct sr_rt
-{
-    struct in_addr dest;
-    struct in_addr gw;
-    struct in_addr mask;
-    char   interface[sr_IFACE_NAMELEN];
-    struct sr_rt* next;
+struct sr_rt {
+  struct in_addr dest;
+  struct in_addr gw;
+  struct in_addr mask;
+  char interface[sr_IFACE_NAMELEN];
+  struct sr_rt *next;
 };
 
+int sr_load_rt(struct sr_instance *, const char *);
+void sr_add_rt_entry(struct sr_instance *, struct in_addr, struct in_addr,
+                     struct in_addr, char *);
+void sr_print_routing_table(struct sr_instance *sr);
+void sr_print_routing_entry(struct sr_rt *entry);
 
-int sr_load_rt(struct sr_instance*,const char*);
-void sr_add_rt_entry(struct sr_instance*, struct in_addr,struct in_addr,
-                  struct in_addr, char*);
-void sr_print_routing_table(struct sr_instance* sr);
-void sr_print_routing_entry(struct sr_rt* entry);
+// added method
+struct sr_rt *find_longest_match(struct sr_instance *, uint32_t);
 
-
-#endif  /* --  sr_RT_H -- */
+#endif /* --  sr_RT_H -- */
