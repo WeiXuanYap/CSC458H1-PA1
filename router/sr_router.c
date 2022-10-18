7274fc727bab61ffaf1d2f46f5668d962d5b0b33
Sean7ee <lee.sean1103@gmail.com>
2022-10-18 02:23:06 -0400

    completed send icmp

--- router/sr_router.c
+++ router/sr_router.c
@@ -1,5 +1,7 @@
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "sr_arpcache.h"
 #include "sr_if.h"
@@ -35,9 +37,30 @@ void sr_init(struct sr_instance *sr) {
 
 } /* -- sr_init -- */
 
+void send_packet(struct sr_instance *sr, uint8_t *p_frame, unsigned int len,
+                 struct sr_if *dest_if, uint32_t dest_ip) {
 
-void send_packet(struct sr_instance* sr, uint8_t* packet, unsigned int len, 
-                  struct sr_if* interface, uint32_t dest_ip) {
+  // check sr->cache to see if arp has been cached then handle accordingly.
+  struct sr_arpentry *cached_ip = sr_arpcache_lookup(&sr->cache, dest_ip);
+  if (cached_ip) {
+    // ip has been cached to a MAC already
+    sr_ethernet_hdr_t *ehdr = (sr_ethernet_hdr_t *)p_frame;
+
+    // set destination MAC to mapped MAC
+    memcpy(ehdr->ether_dhost, cached_ip->mac, ETHER_ADDR_LEN);
+
+    // set source MAC to destination interface's MAC
+    memcpy(ehdr->ether_shost, dest_if->addr, ETHER_ADDR_LEN);
+
+    sr_send_packet(sr, p_frame, len, dest_if->name);
+    free(cached_ip);
+  } else {
+    // not cached; thus, send ARP req
+    struct sr_arpreq *req =
+        sr_arpcache_queuereq(&sr->cache, dest_ip, p_frame, len, dest_if->name);
+    handle_arpreq(sr, req);
+  }
+
   return;
 }
 
@@ -60,29 +83,80 @@ void send_icmp(struct sr_instance *sr, uint8_t *p_frame, unsigned int len,
   struct sr_if *dest_if = sr_get_interface(sr, longest_match->interface);
 
   // divide into all icmp message types and handle them accordingly.
-  switch(type):
-    case echo_reply: {
-      /* set ethernet header source & destination MAC to all 0s */
-      memset(ehdr->ether_dhost, 0, ETHER_ADDR_LEN)
-      memset(ehdr->ether_shost, 0, ETHER_ADDR_LEN)
 
-      /* swap the dst and src ip addresses of ip header*/
-      uint32_t temp = iphdr->ip_dst;
-      iphdr->ip_dst = iphdr->ip_src;
-      iphdr->ip_src = temp;
+  switch (type) {
+  case time_exceeded:
+  case echo_reply: {
+    // set ehdr source MAC and dest MAC to all 0s
+    memset(ehdr->ether_shost, 0, ETHER_ADDR_LEN);
+    memset(ehdr->ether_dhost, 0, ETHER_ADDR_LEN);
 
-      /*construct ICMP Header -> type = 0, code = 0*/
-      sr_icmp_hdr_t* icmp_hdr = (sr_icmp_hdr_t*)(packet + sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t));
-      icmp_hdr->icmp_type = type;
-      icmp_hdr->icmp_code = code;
-      
-      icmp_hdr->icmp_sum = cksum(icmp_hdr, ntohs(ip_hdr->ip_len) - (ip_hdr->ip_hl * 4));
+    // swap destination ip and source ip of iphdr since the packet is being sent
+    // back
+    uint32_t temp = iphdr->ip_dst;
+    iphdr->ip_dst = iphdr->ip_src;
+    iphdr->ip_src = temp;
 
-      send_packet(sr, packet, len, dest_if, longest_match->gw.s_addr);
-      break;
-    }
+    // construct icmp header
+    sr_icmp_hdr_t *icmphdr =
+        (sr_icmp_hdr_t *)(p_frame + sizeof(sr_ethernet_hdr_t) +
+                          sizeof(sr_ip_hdr_t));
+    icmphdr->icmp_type = type;
+    icmphdr->icmp_code = code;
+    icmphdr->icmp_sum =
+        cksum(icmphdr, ntohs(iphdr->ip_len) - (iphdr->ip_hl * 4));
 
+    send_packet(sr, p_frame, len, dest_if, longest_match->gw.s_addr);
+    break;
+  }
+  case dest_unreachable: {
+    unsigned int new_len = (sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t) +
+                            sizeof(sr_icmp_t3_hdr_t));
+    uint8_t *new_frame = malloc(new_len);
 
+    // make ethernet header
+    sr_ethernet_hdr_t *new_ehdr = (sr_ethernet_hdr_t *)new_frame;
+    // make ip header
+    sr_ip_hdr_t *new_iphdr =
+        (sr_ip_hdr_t *)(new_frame + sizeof(sr_ethernet_hdr_t));
+    // make icmp type 3 header
+    sr_icmp_t3_hdr_t *new_icmphdr =
+        (sr_icmp_t3_hdr_t *)(new_frame + sizeof(sr_ethernet_hdr_t) +
+                             (iphdr->ip_hl * 4));
+
+    // new ethernet header source and dest MAC set to 0s
+    memset(new_ehdr->ether_shost, 0, ETHER_ADDR_LEN);
+    memset(new_ehdr->ether_dhost, 0, ETHER_ADDR_LEN);
+    // protocol is IP
+    new_ehdr->ether_type = htons(ethertype_ip);
+
+    // set up iphdr
+    new_iphdr->ip_v = 4;
+    new_iphdr->ip_hl = sizeof(sr_ip_hdr_t) / 4;
+    new_iphdr->ip_tos = 0;
+    new_iphdr->ip_len = htons(sizeof(sr_ip_hdr_t) + sizeof(sr_icmp_t3_hdr_t));
+    new_iphdr->ip_id = htons(0);
+    new_iphdr->ip_off = htons(IP_DF);
+    new_iphdr->ip_ttl = 255;
+    new_iphdr->ip_p = ip_protocol_icmp;
+    // if code is 3, set src IP to received packet's dest_ip
+    new_iphdr->ip_src = (code == port) ? iphdr->ip_dst : dest_if->ip;
+    new_iphdr->ip_dst = iphdr->ip_src;
+    new_iphdr->ip_sum = cksum(new_iphdr, sizeof(sr_ip_hdr_t));
+
+    // set up new_icmphdr
+    new_icmphdr->icmp_type = type;
+    new_icmphdr->icmp_code = code;
+    new_icmphdr->unused = 0;
+    new_icmphdr->next_mtu = 0;
+    memcpy(new_icmphdr->data, iphdr, ICMP_DATA_SIZE);
+    new_icmphdr->icmp_sum = cksum(new_icmphdr, sizeof(sr_icmp_t3_hdr_t));
+
+    send_packet(sr, new_frame, new_len, dest_if, longest_match->gw.s_addr);
+    free(new_frame);
+    break;
+  }
+  }
   return;
 }
 
