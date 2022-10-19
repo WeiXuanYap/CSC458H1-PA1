f9aebbc8bb25a36109096e6c110105dab1e29429
Yap Wei Xuan <e0533997@u.nus.edu>
2022-10-20 06:38:57 +0800

    Add arp_handler, additional function
    added a function to create ethernet headers

--- router/sr_if.c
+++ router/sr_if.c
@@ -43,6 +43,20 @@ struct sr_if* sr_get_interface(struct sr_instance* sr, const char* name)
     return 0;
 } /* -- sr_get_interface -- */
 
+/* Custom function: Get interface via specified IP address */
+struct sr_if* sr_get_interface_by_ip(struct sr_instance* sr, uint32_t ip) 
+{
+    struct sr_if* if_walker = sr->if_list;
+
+    while(if_walker) {
+        if(if_walker->ip == ip) {
+            return if_walker;
+        }
+        if_walker = if_walker->next;
+    }
+    return 0;
+}
+
 /*--------------------------------------------------------------------- 
  * Method: sr_add_interface(..)
  * Scope: Global
